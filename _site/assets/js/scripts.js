193d4acdef5b01e1d39b9222b970a90ff8198557
Robin <info@robinpoort.com>
2025-04-15 21:14:37 +0200

    Make heading clickable on mobile

--- _site/assets/js/scripts.js
+++ _site/assets/js/scripts.js
@@ -158,7 +158,7 @@
     }
     if ($navigationWrapper) {
       $navigationWrapper.addEventListener("click", (ev) => {
-        if (isMobile() && !ev.target.closest(".navigation--persistent")) {
+        if (isMobile() && (ev.target.closest(".navigation__heading") || !ev.target.closest(".navigation--persistent"))) {
           $ui.classList.add("has:toggled-sidebar-left");
         }
       });
